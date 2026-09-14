b11c196ae5bcf9ba3f0408562efc8432495d354f
hanhvn <vonhathanh@gmail.com>
2026-09-14 10:43:03 +0700

    update resize_im() to  handle both color & gray img

--- lib.py
+++ lib.py
@@ -5,10 +5,18 @@ IMG_MIN_WIDTH = 1000.0
 IMG_MIN_HEIGHT = 1000.0
 
 def resize_img(input_img: np.ndarray):
-    height, width = input_img.shape
+    if len(input_img.shape) == 2:
+        height, width = input_img.shape
+    elif len(input_img.shape) == 3:
+        height, width, _ = input_img.shape
+    else:
+        raise ValueError(f'expect input image to has shape = 2 or 3, got: {len(input_img.shape)}')
+
     if width > IMG_MIN_WIDTH:
         ratio = IMG_MIN_WIDTH / width
         width = int(ratio * width)
         height = int(ratio * height)
-    return cv2.resize(input_img, (width, height), interpolation=cv2.INTER_LINEAR)
+    else:
+        ratio = 1.0
+    return cv2.resize(input_img, (width, height), interpolation=cv2.INTER_LINEAR), ratio
 
