71ff15d664d2eaac4ddd939873b1134e15f074d6
hanhvn <vonhathanh@gmail.com>
2026-09-17 21:45:22 +0700

    draw bounding box over currencies

--- scripts/detect_rect.py
+++ scripts/detect_rect.py
@@ -2,21 +2,33 @@ import os
 import sys 
 import cv2
 import glob
-sys.path.append(os.path.abspath('..'))
 from rapidocr import RapidOCR
+
+sys.path.append(os.path.abspath('..'))
+
+
+MINIMUM_CURRENCY_BOX_WIDTH = 100
+MAXIMUM_CURRENCY_BOX_WIDTH = 480
+MINIMUM_CURRENCY_BOX_HEIGHT = 25
+MAXIMUM_CURRENCY_BOX_HEIGHT = 55
+
 
 img_filenames = glob.glob('imgs/*.PNG')
 
 engine = RapidOCR()
+
 for filename in img_filenames:
     img = cv2.imread(filename)
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    result = engine(gray_img)
+    result = engine(img)
 
     for box in result.boxes:
         top_left, top_right, bottom_right, bottom_left = box
-        cv2.rectangle(img, (int(top_left[0]), int(top_left[1])), (int(bottom_right[0]), int(bottom_right[1])), (0, 255, 0), 1)
+        width = int(top_right[0] - top_left[0])
+        height = int(bottom_left[1] - top_left[1])
+        if MAXIMUM_CURRENCY_BOX_WIDTH > width > MINIMUM_CURRENCY_BOX_WIDTH and MAXIMUM_CURRENCY_BOX_HEIGHT > height > MINIMUM_CURRENCY_BOX_HEIGHT:
+            cv2.rectangle(img, (int(top_left[0]), int(top_left[1])), (int(bottom_right[0]), int(bottom_right[1])), (0, 255, 0), 1)
+
     cv2.imshow("output", img)
     cv2.waitKey()
     cv2.destroyAllWindows()
@@ -36,8 +48,3 @@ for filename in img_filenames:
 
 #     def __str__(self):
 #         return f"x={self.x}, y={self.y}, height={self.height}, width={self.width}"
-
-# MINIMUM_CURRENCY_BOX_WIDTH = 120
-# MAXIMUM_CURRENCY_BOX_WIDTH = 500
-# MINIMUM_CURRENCY_BOX_HEIGHT = 30
-# MAXIMUM_CURRENCY_BOX_HEIGHT = 60
