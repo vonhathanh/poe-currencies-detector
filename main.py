d2f2db2a8a67e1baca300ca713a79a47c47eaa9d
hanhvn <vonhathanh@gmail.com>
2026-09-21 10:10:29 +0700

    finish script to test the app

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
-import cv2
 import time
 import numpy as np
 import mss
+import pyautogui
+from rapidocr import RapidOCR
+from lib import *
+
+engine = RapidOCR()
 
 
 def main():
+    currencies = load_currencies_set()
     with mss.MSS() as sct:
         # The monitor or screen part to capture
         monitor = sct.monitors[1]  # or a region
-        for i in range(5):
-            start = time.time()
-            img = np.array(sct.grab(monitor))
-            end = time.time()
-            print(f"Time taken to read image: {end - start}")
+        start = time.time()
+        img = np.array(sct.grab(monitor))
+        end = time.time()
+        print(f"Time taken to read image: {end - start}")
+        result = engine(img)
+        txts = result.txts
+        # enumerate over all bounding boxes
+        for i, box in enumerate(result.boxes):
+            top_left, top_right, _, bottom_left = box
+            width = int(top_right[0] - top_left[0])
+            height = int(bottom_left[1] - top_left[1])
+            # check if the box has valid width & height
+            if MAXIMUM_CURRENCY_BOX_WIDTH > width > MINIMUM_CURRENCY_BOX_WIDTH and MAXIMUM_CURRENCY_BOX_HEIGHT > height > MINIMUM_CURRENCY_BOX_HEIGHT:
+                text = txts[i].lower()
+                # check text has prefix like: 1x, 2x,... nx + currency name
+                match = CURRENCY_PATTERN.match(text)
+                if match:
+                    # remove the prefix to get the original currency name only
+                    end_pos = match.end()
+                    text = text[end_pos:]    
+                if text not in currencies:
+                    continue
+                x = int(top_left[0]) + width // 2
+                y = int(top_left[1]) + height // 2
+                pyautogui.click(x, y)
+                print(f"clicked at {x=}, {y=}")        
+
 
 if __name__ == "__main__":
     main()
--- scripts/detect_rect.py
+++ scripts/detect_rect.py
@@ -2,16 +2,14 @@ import os
 import sys 
 import cv2
 import glob
+import pyautogui
 from rapidocr import RapidOCR
 
 sys.path.append(os.path.abspath('..'))
 
+from lib import *
 
-MINIMUM_CURRENCY_BOX_WIDTH = 100
-MAXIMUM_CURRENCY_BOX_WIDTH = 480
-MINIMUM_CURRENCY_BOX_HEIGHT = 25
-MAXIMUM_CURRENCY_BOX_HEIGHT = 55
-
+currencies = load_currencies_set()
 
 img_filenames = glob.glob('imgs/*.PNG')
 
@@ -22,11 +20,27 @@ for filename in img_filenames:
 
     result = engine(img)
 
-    for box in result.boxes:
+    txts = result.txts
+    # enumerate over all bounding boxes
+    for i, box in enumerate(result.boxes):
         top_left, top_right, bottom_right, bottom_left = box
         width = int(top_right[0] - top_left[0])
         height = int(bottom_left[1] - top_left[1])
+        # check if the box has valid width & height
         if MAXIMUM_CURRENCY_BOX_WIDTH > width > MINIMUM_CURRENCY_BOX_WIDTH and MAXIMUM_CURRENCY_BOX_HEIGHT > height > MINIMUM_CURRENCY_BOX_HEIGHT:
+            text = txts[i].lower()
+            # check text has prefix like: 1x, 2x,... nx + currency name
+            match = CURRENCY_PATTERN.match(text)
+            if match:
+               # remove the prefix to get the original currency name only
+               end_pos = match.end()
+               text = text[end_pos:]    
+            if text not in currencies:
+                continue
+            x = int(top_left[0]) + width // 2
+            y = int(top_left[1]) + height // 2
+            pyautogui.click(x, y)
+            print(f"clicked at {x=}, {y=}")
             cv2.rectangle(img, (int(top_left[0]), int(top_left[1])), (int(bottom_right[0]), int(bottom_right[1])), (0, 255, 0), 1)
 
     cv2.imshow("output", img)
