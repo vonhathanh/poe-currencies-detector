5e3b05d6e0f74f72d6173f8fbd5fae2e665f4992
hanhvn <vonhathanh@gmail.com>
2026-09-22 13:24:02 +0700

    finished app UI & event handler

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import pyautogui
 from pynput import keyboard
 from rapidocr import RapidOCR
 from lib import *
+import flet as ft
 
 engine = RapidOCR()
 currencies = load_currencies_set()
 screen_capture = mss.MSS()
 monitor = screen_capture.monitors[1]  # or a region
-print(f'{monitor=}')
 
 def on_press(key):
     if key == keyboard.Key.space:
@@ -46,15 +46,58 @@ def pickup_currencies():
             print(f"clicked at {x=}, {y=}")        
 
 
-def main():
+def main(page: ft.Page):
+
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
 
-    try:
-        listener.join()
-    except KeyboardInterrupt:
-        listener.stop()
+    page.title = "PoE Detector"
+    page.window_width = 400
+    page.window_height = 300
+    page.padding = 30
 
+    currencies = ft.Checkbox(label="Currencies")
+    base_items = ft.Checkbox(label="Base Items")
+    maps = ft.Checkbox(label="Maps")
+    fragments = ft.Checkbox(label="Fragments")
 
-if __name__ == "__main__":
-    main()
+    def on_change(e):
+        selected = [
+            checkbox.label
+            for checkbox in [currencies, base_items, maps, fragments]
+            if checkbox.value
+        ]
+
+        print("Selected:", selected)
+
+    for checkbox in [currencies, base_items, maps, fragments]:
+        checkbox.on_change = on_change
+
+    page.add(
+        ft.Column(
+            [
+                ft.Text(
+                    "PoE Currency Detector",
+                    size=24,
+                    weight=ft.FontWeight.BOLD,
+                ),
+                ft.Divider(),
+                currencies,
+                base_items,
+                maps,
+                fragments,
+            ],
+            spacing=10,
+        )
+    )
+
+    def on_window_event(e):
+        if e.type == ft.WindowEventType.CLOSE:
+            print("App is exiting!")
+            listener.stop()
+            listener.join()
+            print('App exited')
+
+    page.window.on_event = on_window_event
+
+ft.run(main)
