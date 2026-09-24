6ed6e128dd34679959b10e396f66cb8b6c33199b
hanhvn <vonhathanh@gmail.com>
2026-09-24 22:47:50 +0700

    significantly improve processing time by split the process into two phases

--- scripts/detect_rect.py
+++ scripts/detect_rect.py
@@ -1,9 +1,9 @@
 import os
 import sys 
 import cv2
+import time
 import glob
-import pyautogui
-from rapidocr import RapidOCR
+from paddleocr import TextDetection, TextRecognition
 
 sys.path.append(os.path.abspath('..'))
 
@@ -13,52 +13,28 @@ currencies = load_currencies_set()
 
 img_filenames = glob.glob('imgs/*.PNG')
 
-engine = RapidOCR()
+text_detection_model = TextDetection(engine='transformers')
+text_recognition_model = TextRecognition(engine='transformers')
 
 for filename in img_filenames:
+
+    print('processing ', filename)
+
     img = cv2.imread(filename)
 
-    result = engine(img)
+    boxes = text_detection_model.predict(img)[0]["dt_polys"]
 
-    txts = result.txts
-    # enumerate over all bounding boxes
-    for i, box in enumerate(result.boxes):
-        top_left, top_right, bottom_right, bottom_left = box
+    valid_boxes = []
+
+    for box in boxes:
+        top_left, top_right, _, bottom_left = box
         width = int(top_right[0] - top_left[0])
         height = int(bottom_left[1] - top_left[1])
         # check if the box has valid width & height
         if MAXIMUM_CURRENCY_BOX_WIDTH > width > MINIMUM_CURRENCY_BOX_WIDTH and MAXIMUM_CURRENCY_BOX_HEIGHT > height > MINIMUM_CURRENCY_BOX_HEIGHT:
-            text = txts[i].lower()
-            # check text has prefix like: 1x, 2x,... nx + currency name
-            match = CURRENCY_PATTERN.match(text)
-            if match:
-               # remove the prefix to get the original currency name only
-               end_pos = match.end()
-               text = text[end_pos:]    
-            if text not in currencies:
-                continue
-            x = int(top_left[0]) + width // 2
-            y = int(top_left[1]) + height // 2
-            pyautogui.click(x, y)
-            print(f"clicked at {x=}, {y=}")
-            cv2.rectangle(img, (int(top_left[0]), int(top_left[1])), (int(bottom_right[0]), int(bottom_right[1])), (0, 255, 0), 1)
-
-    cv2.imshow("output", img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-
-# class Rect:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
-#         self.height = 1
-#         self.width = 1
-
-#     def set_height(self, value):
-#         self.height = value
-
-#     def set_width(self, value):
-#         self.width = value
-
-#     def __str__(self):
-#         return f"x={self.x}, y={self.y}, height={self.height}, width={self.width}"
+            cropped_region = img[top_left[1]:bottom_left[1], top_left[0]:top_right[0], :]
+            valid_boxes.append(cropped_region)
+    start = time.time()
+    txts = text_recognition_model.predict(valid_boxes, batch_size=32)
+    end = time.time()
+    print('cost ', end - start)
